f3a4a03165250ffca023e35dc1df907a354ded46
Othman <dev@leventures.com>
2023-09-07 12:53:32 +0100

    fix z-index

--- src/components/SwitchTheme.vue
+++ src/components/SwitchTheme.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-40 z-10 w-11 h-11 bg-white dark:bg-dark-100 rounded-tr-lg rounded-br-lg border border-dark-75 border-l-0 border-opacity-70 dark:border-white dark:border-opacity-10">
+  <div class="fixed top-40 z-50 w-11 h-11 bg-white dark:bg-dark-100 rounded-tr-lg rounded-br-lg border border-dark-75 border-l-0 border-opacity-70 dark:border-white dark:border-opacity-10">
     <span @click="switchTheme(isDark)" class="w-full h-full cursor-pointer flex items-center justify-center text-black dark:text-white transition-colors duration-200 hover:text-primary dark:hover:text-primary">
       <LightIcon v-if="!isDark" class="w-5 h-5 spinner" />
       <DarkIcon v-else class="w-5 h-5 spinner" />
